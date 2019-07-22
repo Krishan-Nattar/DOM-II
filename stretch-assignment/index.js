5bc8e9b4585cfb43ec60b2ff06b1f112e309ef49
Krishan Nattar <krishan.nattar@gmail.com>
2019-07-22 11:11:47 -0700

    Color rearranging works!

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -21,98 +21,108 @@ blocks.forEach(function(item, index, array) {
   });
 });
 
+function boxArrange(r, b, gre, p, g) {
+  if (topBox == red) {
+    TweenMax.to(topBox, 3, { transform: `translateY(${r}px)` });
+  } else if (topBox == blue) {
+    TweenMax.to(topBox, 3, { transform: `translateY(${b}px)` });
+  } else if (topBox == green) {
+    TweenMax.to(topBox, 3, { transform: `translateY(${gre}px)` });
+  } else if (topBox == pink) {
+    TweenMax.to(topBox, 3, { transform: `translateY(${p}px)` });
+  } else if (topBox == gray) {
+    TweenMax.to(topBox, 3, { transform: `translateY(${g}px)` });
+  }
+}
+
 function moveBoxes(color) {
   if (color == topBox) {
-    //
     //Do nothing!
   } else if (color == secondBox) {
     if (color == red) {
       TweenMax.to(color, 3, { transform: "translateY(0px)" });
-
-      if (topBox == blue) {
-        TweenMax.to(topBox, 3, { transform: "translateY(0px)" });
-      }else if (topBox == green) {
-        TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-      } else if (topBox == pink) {
-        TweenMax.to(topBox, 3, { transform: "translateY(240px)" });
-      } else if (topBox == gray) {
-        TweenMax.to(topBox, 3, { transform: "translateY(360px)" });
-      }
-
+      boxArrange(120, 0, -120, -240, -360);
     } else if (color == blue) {
       TweenMax.to(color, 3, { transform: "translateY(-120px)" });
-
-        if(topBox ==red){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==green){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==pink){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==gray){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        }
-      
-    } else if (color==green){
-        TweenMax.to(color, 3, { transform: "translateY(-120px)" });
-
-        if(topBox ==red){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==blue){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==pink){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==gray){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        }
-    } else if (color==pink){
-        TweenMax.to(color, 3, { transform: "translateY(-120px)" });
-
-        if(topBox ==red){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==blue){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==green){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==gray){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        }
-    } else if (color==gray){
-        TweenMax.to(color, 3, { transform: "translateY(-120px)" });
-
-        if(topBox ==red){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==blue){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==green){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        } else if(topBox ==pink){
-            TweenMax.to(topBox, 3, { transform: "translateY(120px)" });
-        }
+      boxArrange(120, 0, -120, -240, -360);
+    } else if (color == green) {
+      TweenMax.to(color, 3, { transform: "translateY(-240px)" });
+      boxArrange(120, 0, -120, -240, -360);
+    } else if (color == pink) {
+      TweenMax.to(color, 3, { transform: "translateY(-360px)" });
+      boxArrange(120, 0, -120, -240, -360);
+    } else if (color == gray) {
+      TweenMax.to(color, 3, { transform: "translateY(-480px)" });
+      boxArrange(120, 0, -120, -240, -360);
     }
 
     secondBox = switchToTop(color);
   } else if (color == thirdBox) {
+
+
     if (color == red) {
       TweenMax.to(color, 3, { transform: "translateY(0px)" });
-      if (topBox == green) {
-        TweenMax.to(topBox, 3, { transform: "translateY(0px)" });
-      }
+      boxArrange(240, 120, 0, 120, -240);
+    } else if (color == blue) {
+      TweenMax.to(color, 3, { transform: "translateY(-120px)" });
+      boxArrange(240, 120, 0, 120, -240);
     } else if (color == green) {
-      TweenMax.to(topBox, 3, { transform: "translateY(240px)" });
       TweenMax.to(color, 3, { transform: "translateY(-240px)" });
+      boxArrange(240, 120, 0, 120, -240);
+      console.log(color, " ", topBox);
+    } else if (color == pink) {
+      TweenMax.to(color, 3, { transform: "translateY(-360px)" });
+      boxArrange(240, 120, 0, 120, -240);
+    } else if (color == gray) {
+      TweenMax.to(color, 3, { transform: "translateY(-480px)" });
+      boxArrange(240, 120, 0, 120, -240);
     }
 
     thirdBox = switchToTop(color);
   } else if (color == fourthBox) {
-    // console.log("current color is ",color);
-    TweenMax.to(topBox, 3, { transform: "translateY(360px)" });
-    TweenMax.to(color, 3, { transform: "translateY(-360px)" });
+    if (color == red) {
+      TweenMax.to(color, 3, { transform: "translateY(0px)" });
+      boxArrange(360, 240, 120, 0, -120);
+    } else if (color == blue) {
+      TweenMax.to(color, 3, { transform: "translateY(-120px)" });
+      boxArrange(360, 240, 120, 0, -120);
+    } else if (color == green) {
+      TweenMax.to(color, 3, { transform: "translateY(-240px)" });
+      boxArrange(360, 240, 120, 0, -120);
+    } else if (color == pink) {
+      TweenMax.to(color, 3, { transform: "translateY(-360px)" });
+      boxArrange(360, 240, 120, 0, -120);
+    } else if (color == gray) {
+      TweenMax.to(color, 3, { transform: "translateY(-480px)" });
+      boxArrange(360, 240, 120, 0, -120);
+    }
+
     fourthBox = switchToTop(color);
+    console.log(topBox);
   } else if (color == fifthBox) {
-    // console.log("current color is ",color);
-    TweenMax.to(topBox, 3, { transform: "translateY(480px)" });
-    TweenMax.to(color, 3, { transform: "translateY(-480px)" });
-    fifthBox = switchToTop(color);
+    if (color == red) {
+        TweenMax.to(color, 3, { transform: "translateY(0px)" });
+        boxArrange(480, 360, 240, 120, 0);
+        
+      } else if (color == blue) {
+        TweenMax.to(color, 3, { transform: "translateY(-120px)" });
+        boxArrange(480, 360, 240, 120, 0);
+        
+      } else if (color == green) {
+        TweenMax.to(color, 3, { transform: "translateY(-240px)" });
+        boxArrange(480, 360, 240, 120, 0);
+        
+      } else if (color == pink) {
+        TweenMax.to(color, 3, { transform: "translateY(-360px)" });
+        boxArrange(480, 360, 240, 120, 0);
+        
+      } else if (color == gray) {
+        TweenMax.to(color, 3, { transform: "translateY(-480px)" });
+        boxArrange(480, 360, 240, 120, 0);
+        
+      }
+  
+      fifthBox = switchToTop(color);
   }
 }
 
@@ -130,7 +140,6 @@ function switchToTop(current) {
     topBox = current;
     return pink;
   } else if (topBox == gray) {
-    // console.log('hi');
     topBox = current;
     return gray;
   }
